952dde6fbbe791252d8ac2e9c32cb64937da3440
harshajxn <93381030+harshajxn@users.noreply.github.com>
2025-05-21 13:53:45 +0300

    Updated app.py to handle new form input validation

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,3 @@
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return '''
-#     <h1>PricePulse Amazon Tracker</h1>
-#     <form method="POST" action="/track">
-#       <input type="text" name="url" placeholder="Enter Amazon Product URL" size="50" required>
-#       <button type="submit">Track</button>
-#     </form>
-#     '''
-
-# @app.route('/track', methods=['POST'])
-# def track():
-#     url = request.form['url']
-#     return f'You entered: {url}'
-
-# if __name__ == '__main__':
-#     app.run(debug=True) 
-
 from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
